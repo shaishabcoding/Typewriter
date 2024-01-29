fb0c3bf76e800466ffb9eaf3536f6c17167d37fa
shaishabcoding <myshaishab@gmail.com>
2024-01-29 22:04:07 +0600

    added font

--- main.js
+++ main.js
@@ -2,51 +2,40 @@ const $ = (e, p = document) => p.querySelectorAll(e);
 let timer;
 
 function main() {
-  const hidden_containers = $('.hidden_container');
+  const hidden_containers = $(".hidden_container");
   [...hidden_containers].forEach((p) => {
     const hidden_parent = p.parentElement,
-      isHidden = $('.isHidden', hidden_parent)[0],
-      hidden_text = $('.hidden_text', p)[0],
-      hidden_input = $('.hidden_input', p)[0];
+      isHidden = $(".isHidden", hidden_parent)[0],
+      hidden_text = $(".hidden_text", p)[0],
+      hidden_input = $(".hidden_input", p)[0];
     let isHiddenState = true;
 
     isHidden.onclick = () => {
-      if (isHiddenState)
-        showText(hidden_input, hidden_text);
-
-      else
-        removeText(hidden_input);
-
-
+      if (isHiddenState) showText(hidden_input, hidden_text);
+      else removeText(hidden_input);
 
       isHiddenState = !isHiddenState;
-    }
+    };
 
-    hidden_input.style.width = hidden_text.clientWidth + 'px';
-    hidden_input.style.height = hidden_text.clientHeight + 'px';
-    setTimeout(
-      () => {
-        hidden_text.style.display = 'none';
-      }, 1
-    );
-    hidden_input.onfocus = () => {
-      hidden_input.classList.add("simple_text")
-    }
+    hidden_input.style.width = hidden_text.clientWidth + "px";
+    hidden_input.style.height = hidden_text.clientHeight + "px";
+    setTimeout(() => {
+      hidden_text.style.display = "none";
+    }, 1);
+    hidden_input.onClick = () => {
+      hidden_input.classList.add("simple_text");
+    };
     hidden_input.addEventListener("keyup", (event) => {
       isHidden.checked = false;
       isHiddenState = true;
-      if (!hidden_input.value)
-        hidden_input.style.borderColor = "black";
+      if (!hidden_input.value) hidden_input.style.borderColor = "black";
 
       if (hidden_input.value === hidden_text.innerHTML.trim()) {
         isHidden.checked = true;
         isHidden.disabled = true;
         hidden_input.disabled = true;
       }
-
     });
-
-
   });
 }
 
@@ -67,14 +56,12 @@ function showText(e, p) {
       timer = setTimeout(typeWriter, speed);
     }
   })();
-};
+}
 
 function removeText(e) {
   clearTimeout(timer);
   e.value = "";
   e.style.borderColor = "black";
-
 }
 
-
-main()
+main();
